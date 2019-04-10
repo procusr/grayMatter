c01934fd481924b58e03787fcc7184fc23f4bfa1
Faris <procusr@gmail.com>
2019-04-10 18:40:19 +0200

    "added Dropdown with test commune names"

--- app/src/main/java/com/example/myapplication/Main.java
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Main extends AppCompatActivity {
      private String companyName = "Novare";
@@ -16,6 +19,36 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner myCommuneList = (Spinner) findViewById(R.id.dd_commune_list);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1
+                , getResources().getStringArray(R.array.Communes));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        myCommuneList.setAdapter(myAdapter);
+
+
+
+        myCommuneList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                    @Override
+                                                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                                        if (i == 1) {
+
+                                                        } else if (i == 2) {
+
+                                                        } else if (i == 3) {
+
+                                                        } else if (i == 4) {
+                                                        }
+                                                    }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         saveData();
     }
 
@@ -50,12 +83,8 @@ public class Main extends AppCompatActivity {
                 actualTax = mActualTax.getText().toString().trim();
                 Company company =new Company(companyName,Integer.parseInt(salary),Integer.parseInt(expectedTax),Integer.parseInt(actualTax));
                 //TODO Database reference
-
             }
         });
-
-
-
     }
 }
 
