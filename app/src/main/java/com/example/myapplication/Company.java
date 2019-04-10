e4827ef86cdb44b0395795c5c6ab5866847473bc
Faris <procusr@gmail.com>
2019-04-10 18:18:16 +0200

    "fixed"

--- app/src/main/java/com/example/myapplication/Company.java
+++ app/src/main/java/com/example/myapplication/Company.java
@@ -1,4 +1,58 @@
 package com.example.myapplication;
 
 public class Company {
+    private String companyName ;
+    private int expectedTax;
+
+    public Company(String companyName, int expectedTax, int actualTax, int difference) {
+        this.companyName = companyName;
+        this.expectedTax = expectedTax;
+        this.actualTax = actualTax;
+        this.difference = difference;
+    }
+
+    public Company(){
+    }
+
+    public Company(String companyName, int expectedTax, int actualTax) {
+        this.companyName = companyName;
+        this.expectedTax = expectedTax;
+        this.actualTax = actualTax;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getExpectedTax() {
+        return expectedTax;
+    }
+
+    public void setExpectedTax(int expectedTax) {
+        this.expectedTax = expectedTax;
+    }
+
+    public int getActualTax() {
+        return actualTax;
+    }
+
+    public void setActualTax(int actualTax) {
+        this.actualTax = actualTax;
+    }
+
+    public int getDifference() {
+        return difference;
+    }
+
+    public void setDifference(int difference) {
+        this.difference = difference;
+    }
+
+    private int actualTax;
+    private int difference;
+
 }
--- app/src/main/java/com/example/myapplication/Main.java
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -2,12 +2,60 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends AppCompatActivity {
+     private String companyName = "Novare";
+     private String expectedTax="30000";
+     private String actualTax="2000";
+     private String salary="1000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        saveData();
+    }
+
+    public void saveData(){
+
+        final EditText mCompanyName=findViewById(R.id.PT_Company_name);
+        final EditText mSalary=findViewById(R.id.PT_Salary);
+        final EditText mExpectedTax=findViewById(R.id.TV_tax);
+        final EditText mActualTax = findViewById(R.id.TV_Salary_afterTax);
+
+        mCompanyName.setText(companyName);
+        mCompanyName.setSelection(companyName.length());
+
+        mSalary.setText(salary);
+        mSalary.setSelection(salary.length());
+
+        mExpectedTax.setText(expectedTax);
+        mExpectedTax.setSelection(expectedTax.length());
+
+        mActualTax.setText(actualTax);
+        mActualTax.setSelection(actualTax.length());
+
+
+        Button save = findViewById(R.id.B_Save_db);
+
+       save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                companyName=mCompanyName.getText().toString().trim();
+                salary=mSalary.getText().toString().trim();
+                expectedTax= mExpectedTax.getText().toString().trim();
+                actualTax = mActualTax.getText().toString().trim();
+                Company company =new Company(companyName,Integer.parseInt(salary),Integer.parseInt(expectedTax),Integer.parseInt(actualTax));
+                //TODO Database reference
+
+            }
+        });
+
+
+
     }
 }
+
