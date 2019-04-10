abf01c1f545f9c373a1e183ab49ca05fb9667fe7
fadimashan <fadi.mashan@gmail.com>
2019-04-10 13:00:01 +0200

    change the package

---
+++ app/src/main/java/com/example/mytax/ForgetAndChangePasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.mytax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgetAndChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_and_change_password);
+    }
+}
