6a21699f050086b30e2fc120afe084bd14aa4a9e
fadimashan <fadi.mashan@gmail.com>
2019-04-16 13:31:37 +0200

    finish the options activities (car, salary, house percentage, plus and summary)

--- app/src/main/java/com/example/mytax/MainActivity.java
+++ app/src/main/java/com/example/mytax/MainActivity.java
@@ -25,4 +25,24 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, PopMain.class));
     }
 
+
+    public void Btn_car(View view) { Intent intent = new Intent(MainActivity.this, car.class);
+    startActivity(intent);}
+
+    public void house(View view) {Intent intent =new Intent(MainActivity.this, house.class);
+        startActivity(intent);}
+
+    public void summary(View view) {Intent intent = new Intent(MainActivity.this, summary.class);
+        startActivity(intent);}
+
+    public void percentage(View view) {Intent intent = new Intent(MainActivity.this, percentage.class);
+        startActivity(intent);}
+
+    public void salary(View view) {Intent intent = new Intent(MainActivity.this, Salary.class);
+        startActivity(intent);}
+
+    public void plus(View view) {Intent intent = new Intent(MainActivity.this, plus.class);
+        startActivity(intent);}
+
+
 }
