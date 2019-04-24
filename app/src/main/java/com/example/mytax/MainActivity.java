a122c1e8d100e6452774e020a0c4b7b2b958cdce
catalin.moldovan86@gmail.com <catalin.moldovan86@gmail.com>
2019-04-24 14:08:00 +0200

    app introduction functionality

--- app/src/main/java/com/example/mytax/MainActivity.java
+++ app/src/main/java/com/example/mytax/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.mytax;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout l1,l2;
+    Button welcome;
+    Animation uptodown,downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        welcome = findViewById(R.id.welcome);
+//
+//        setContentView(R.layout.welcome_activity);
+//        l1 = findViewById(R.id.l1);
+//        l2 = findViewById(R.id.l2);
+//        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+//        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+//        l1.setAnimation(uptodown);
+//        l2.setAnimation(downtoup);
 
+//        welcome.setAnimation(downtoup);
 
     }
     public void setting (View view){
@@ -50,5 +68,14 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, Plus.class);
         startActivity(intent);}
 
-
+    public void main(View view)
+    {
+        Intent intent = new Intent(MainActivity.this , MainActivity.class);
+        startActivity(intent);
+    }
+    public void login(View view)
+    {
+        Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+        startActivity(intent);
+    }
 }
