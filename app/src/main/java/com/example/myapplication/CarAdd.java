275edaeeddfadf3bfc252ba4bbc204f27ee38136
Hera <hera@n171-p237.eduroam.kth.se>
2019-04-25 17:24:36 +0200

    Final version

--- app/src/main/java/com/example/myapplication/CarAdd.java
+++ app/src/main/java/com/example/myapplication/CarAdd.java
@@ -10,30 +10,62 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.google.firebase.firestore.CollectionReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 
+import static com.example.myapplication.R.id.save_car;
+import static com.example.myapplication.R.id.text_view_endDate;
+
 public class CarAdd extends AppCompatActivity {
-private TextView distance ;
-private TextView out;
+    private EditText editTextdistance;
+    private TextView textViewstartDate;
+    private TextView textViewendDate;
+    private Switch   switchgpsDistance;
+    private EditText editTextorigin;
+    private EditText editTextdestination;
+    private EditText editTextpurpose;
+    private EditText editTextamount;
+
+
+
 private static final String TAG = "CarAdd";
-    private TextView mDisplayDate;
+
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-    private TextView eDisplayDate;
+    private DatePickerDialog.OnDateSetListener eDateSetListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_add);
-        distance = findViewById(R.id.text_input_distance);
-        out = findViewById(R.id.Amount);
-        mDisplayDate = findViewById(R.id.tvDate);
-        eDisplayDate= findViewById(R.id.endDate);
 
-        distance.addTextChangedListener(new TextWatcher() {
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Car Info");
+
+
+
+        editTextdistance = findViewById(R.id.edit_text_distance);
+        textViewstartDate = findViewById(R.id.text_view_startDate);
+        textViewendDate = findViewById(text_view_endDate);
+        switchgpsDistance = findViewById(R.id.switch_gpsDistance);
+        editTextorigin = findViewById(R.id.edit_text_origin);
+        editTextdestination = findViewById(R.id.edit_text_destination);
+        editTextpurpose = findViewById(R.id.edit_text_purpose);
+        editTextamount = findViewById(R.id.edit_text_amount);
+
+
+        editTextdistance.addTextChangedListener(new TextWatcher() {
 
             // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
@@ -46,13 +78,13 @@ private static final String TAG = "CarAdd";
 
             public void afterTextChanged(Editable c) {
                 // this one too
-                if(!validateDistance()){
+                if (!validateDistance()) {
                     return;
                 }
-                String edistance = distance.getText().toString();
+                String edistance = editTextdistance.getText().toString();
 
                 double k = Double.parseDouble(edistance);
-                out.setText(" " +k*1.85+ ".kr");
+                editTextamount.setText(" " + k * 1.85 + ".kr");
 
             }
 
@@ -60,8 +92,7 @@ private static final String TAG = "CarAdd";
         });
 
 
-
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+        textViewstartDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar calendar = Calendar.getInstance();
@@ -79,7 +110,7 @@ private static final String TAG = "CarAdd";
             }
         });
 
-        eDisplayDate.setOnClickListener(new View.OnClickListener() {
+        textViewendDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar calendar = Calendar.getInstance();
@@ -89,16 +120,13 @@ private static final String TAG = "CarAdd";
 
                 DatePickerDialog dialog = new DatePickerDialog(
                         CarAdd.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener, year, month, day);
+                        eDateSetListener, year, month, day);
 
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
 
             }
         });
-
-
-
 
 
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -107,43 +135,91 @@ private static final String TAG = "CarAdd";
                 month = month + 1;
                 Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
                 String date = month + "/" + day + "/" + year;
-                mDisplayDate.setText(date);
-                eDisplayDate.setText(date);
-            }
-            };
+                textViewstartDate.setText(date);
 
+            }
+        };
+        eDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year;
+                textViewendDate.setText(date);
+
+            }
+
+
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.new_car_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_car:
+                saveCar();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean validateDistance() {
+        if (editTextdistance.getText().toString().isEmpty()) {
+
+            editTextdistance.setError("Filed can not be Empty");
+            return false;
+        } else if (Double.parseDouble(editTextdistance.getText().toString()) < 5) {
+
+            editTextdistance.setError("Distance entered should be more than 5 killometers");
+            return false;
+        } else if (textViewstartDate.getText().toString().isEmpty()) {
+
+            textViewstartDate.setError("Filed can not be Empty");
+            return false;
+        } else {
+
+            editTextdistance.setError(null);
+            return true;
         }
 
 
+    }
 
 
-      private boolean validateDistance() {
-          if (distance.getText().toString().isEmpty()) {
-              System.out.println("qw");
-              distance.setError("Filed can not be Empty");
-              return false;
-          } else if (Double.parseDouble(distance.getText().toString()) < 5) {
-              System.out.println("qqqq");
-              distance.setError("Distance entered should be more than 5 killometers");
-              return false;
-          } else {
-              System.out.println("xxxx");
-              distance.setError(null);
-              return true;
-
-          }
-
-      }
-
-   /*public void saveInput(View v){
+   public void saveCar(){
        if(!validateDistance()){
            return;
       }
-      String edistance = distance.getText().toString();
+      String distance = editTextdistance.getText().toString();
+       String startDate= textViewstartDate.getText().toString();
+       String endDate= textViewendDate.getText().toString();
+       String  origin = editTextorigin.getText().toString();
+       String destination = editTextdestination.getText().toString();
+       String purpose = editTextpurpose.getText().toString();
+       String amount = editTextamount.getText().toString();
 
-       double k = Double.parseDouble(edistance);
-      out.setText(" " +k*1.85+ ".kr");
+      // CollectionReference carrRef = FirebaseFirestore.getInstance().
+               //collection("CarInfo");
 
-    }*/
+      // carrRef.add(new CarInfo(distance, startDate, endDate, origin, destination, purpose, amount));
+
+       Toast.makeText(this , " Saved", Toast.LENGTH_SHORT).show();
+       finish();
+       /*double k = Double.parseDouble(edistance);
+      out.setText(" " +k*1.85+ ".kr");*/
+
+    }
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,13 +20,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
@@ -34,6 +35,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     new SalaryFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_salary);
         }*/
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        ;
     }
 
     @Override
@@ -50,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
               break;
 */
             case R.id.nav_car:
-                Intent intent = new Intent(this, CarAdd.class);
+                Intent intent = new Intent(MainActivity.this, CarMain.class);
                 startActivity(intent);
                 finish();
                 break;
@@ -89,6 +96,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 finish();*/
 
         }
+
+
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
